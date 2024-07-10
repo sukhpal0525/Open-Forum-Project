7063eb12a00ab901d6c02274a2f0996608cbceae
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-07-10 23:52:02 +0100

    Add DB context

---
+++ Forum/Forum/Data/ForumContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Forum.Models;
+
+namespace ForumApp.Data
+{
+    public class ForumContext : DbContext
+    {
+        public ForumContext() : base("ForumContext")
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Forum/Forum/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
