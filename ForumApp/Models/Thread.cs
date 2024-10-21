1c4114c2d3993856c6ada5bcfb79c0936c584fd4
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-10-21 23:00:08 +0100

    Define entity relationship for Thread & Category (foreign key & navigation property)

--- ForumApp/Models/Thread.cs
+++ ForumApp/Models/Thread.cs
@@ -6,4 +6,8 @@ public class Thread
     public int ViewsCount { get; set; }
     public User? Author { get; set; }
     public List<Post> Posts { get; set; } = new List<Post>();
+
+    public int CategoryId {get; set; }
+
+    public Category Category {get; set; }
 }
