6b803fc24b7fd853fdda2709575e35515cada24e
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-03 16:59:40 +0000

    Add author foreign key for Thread

--- ForumApp/Models/Thread.cs
+++ ForumApp/Models/Thread.cs
@@ -4,10 +4,13 @@ public class Thread
     public string Title { get; set; } = string.Empty;
     public int RepliesCount { get; set; }
     public int ViewsCount { get; set; }
+
+
     public User? Author { get; set; }
-    public List<Post> Posts { get; set; } = new List<Post>();
+    public int AuthorId { get; set; }
 
     public int CategoryId {get; set; }
-
     public Category Category {get; set; }
+    public List<Post> Posts { get; set; } = new List<Post>();
+
 }
