83f3eb365cb770a109f02273e701c7e975e65827
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-13 23:48:08 +0000

    Use data annotations for Post model

--- ForumApp/Models/Post.cs
+++ ForumApp/Models/Post.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Post
 {
     public int Id { get; set; }
+
+    [Required]
     public string Content { get; set; }
+    
     public DateTime CreatedDate { get; set; }
     
     public int ThreadId { get; set; }
