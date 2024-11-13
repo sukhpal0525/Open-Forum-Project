bf83c15df39a7f0bc3a1be0f95bd37b77e01bef4
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-13 23:46:59 +0000

    Add data annotations (required & max length) for Thread's title field

--- ForumApp/Models/Thread.cs
+++ ForumApp/Models/Thread.cs
@@ -1,16 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Thread
 {
     public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; }
+    
     public int RepliesCount { get; set; }
     public int ViewsCount { get; set; }
 
-
-    public User? Author { get; set; }
-    public int AuthorId { get; set; }
-
+    public int AuthorId { get; set; }    
     public int CategoryId {get; set; }
     public Category Category {get; set; }
-    public List<Post> Posts { get; set; } = new List<Post>();
 
+    public List<Post> Posts { get; set; } = new List<Post>();
 }
