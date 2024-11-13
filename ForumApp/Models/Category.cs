5261340ac25a157aa498927f187c12d3bb9773bc
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-13 23:48:41 +0000

    Add required & max length annotations to Category model

--- ForumApp/Models/Category.cs
+++ ForumApp/Models/Category.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Category
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
+
+    [StringLength(100)]
     public string Description { get; set; }
+    
     public List<Thread> Threads { get; set; } = new List<Thread>();
 }
