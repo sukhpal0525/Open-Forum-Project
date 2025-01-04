c6520a6385329ca54356e62d084ac23e0f84dc84
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-01-04 23:17:55 +0000

    Rebuild project solution (docker build)

--- ForumApp/obj/Debug/net8.0/ForumApp.AssemblyInfo.cs
+++ ForumApp/obj/Debug/net8.0/ForumApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForumApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8a701a146e8337d14352137c33c1e517615517")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3827788b9fd1041488b4e3fc640ebd82ea844e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForumApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForumApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
