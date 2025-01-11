5e529417a4fc5654b7994262a42298e32b890f09
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-01-11 22:30:02 +0000

    Make AvatarUrl field nullable

--- ForumApp/Models/User.cs
+++ ForumApp/Models/User.cs
@@ -3,6 +3,6 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; }
     public string Role { get; set; }  // Admin, Moderator, Member
-    public string AvatarUrl { get; set; }
+    public string? AvatarUrl { get; set; }
     public ICollection<Post> Posts { get; set; }
 }
