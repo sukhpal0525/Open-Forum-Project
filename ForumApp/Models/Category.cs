8e0ff9875bba5e2d1084297e6e26d14b57850150
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-01-25 22:34:50 +0000

    Edit Name length constraint in Category

--- ForumApp/Models/Category.cs
+++ ForumApp/Models/Category.cs
@@ -5,7 +5,7 @@ public class Category
     public int Id { get; set; }
 
     [Required]
-    [StringLength(100)]
+    [StringLength(50)]
     public string Name { get; set; }
 
     [StringLength(100)]
