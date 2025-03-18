982034ec4d18f360bf9783f4e7951ca190481de7
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-03-18 23:56:31 +0000

    Drop database & remove all migrations

--- ForumApp/Migrations/ApplicationDbContextModelSnapshot.cs
+++ ForumApp/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -25,15 +25,83 @@ namespace ForumApp.Migrations
 
                     b.Property<string>("Description")
                         .IsRequired()
+                        .HasMaxLength(100)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Name")
                         .IsRequired()
+                        .HasMaxLength(50)
                         .HasColumnType("TEXT");
 
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Description = "Talk about anything here",
+                            Name = "General Discussion"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Description = "Official announcements from the team",
+                            Name = "Announcements"
+                        });
+                });
+
+            modelBuilder.Entity("ForumThread", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int?>("AuthorId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int?>("CategoryId")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("RepliesCount")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .HasColumnType("TEXT");
+
+                    b.Property<int>("ViewsCount")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AuthorId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("ForumThreads");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            AuthorId = 1,
+                            CategoryId = 1,
+                            RepliesCount = 5,
+                            Title = "Welcome to the Forum",
+                            ViewsCount = 100
+                        },
+                        new
+                        {
+                            Id = 2,
+                            AuthorId = 2,
+                            CategoryId = 2,
+                            RepliesCount = 3,
+                            Title = "Forum Rules",
+                            ViewsCount = 50
+                        });
                 });
 
             modelBuilder.Entity("Post", b =>
@@ -52,47 +120,16 @@ namespace ForumApp.Migrations
                     b.Property<DateTime>("CreatedDate")
                         .HasColumnType("TEXT");
 
-                    b.Property<int>("ThreadId")
+                    b.Property<int>("ForumThreadId")
                         .HasColumnType("INTEGER");
 
                     b.HasKey("Id");
 
                     b.HasIndex("AuthorId");
 
-                    b.HasIndex("ThreadId");
+                    b.HasIndex("ForumThreadId");
 
                     b.ToTable("Posts");
-                });
-
-            modelBuilder.Entity("Thread", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
-
-                    b.Property<int?>("AuthorId")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<int>("CategoryId")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<int>("RepliesCount")
-                        .HasColumnType("INTEGER");
-
-                    b.Property<string>("Title")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
-                    b.Property<int>("ViewsCount")
-                        .HasColumnType("INTEGER");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("AuthorId");
-
-                    b.HasIndex("CategoryId");
-
-                    b.ToTable("Threads");
                 });
 
             modelBuilder.Entity("User", b =>
@@ -102,24 +139,50 @@ namespace ForumApp.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("AvatarUrl")
-                        .IsRequired()
+                        .HasMaxLength(500)
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Role")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<string>("Signature")
-                        .IsRequired()
-                        .HasColumnType("TEXT");
-
                     b.Property<string>("Username")
                         .IsRequired()
+                        .HasMaxLength(50)
                         .HasColumnType("TEXT");
 
                     b.HasKey("Id");
 
                     b.ToTable("Users");
+
+                    b.HasData(
+                        new
+                        {
+                            Id = 1,
+                            Role = "Admin",
+                            Username = "Administrator"
+                        },
+                        new
+                        {
+                            Id = 2,
+                            Role = "Member",
+                            Username = "John"
+                        });
+                });
+
+            modelBuilder.Entity("ForumThread", b =>
+                {
+                    b.HasOne("User", "Author")
+                        .WithMany()
+                        .HasForeignKey("AuthorId");
+
+                    b.HasOne("Category", "Category")
+                        .WithMany("ForumThreads")
+                        .HasForeignKey("CategoryId");
+
+                    b.Navigation("Author");
+
+                    b.Navigation("Category");
                 });
 
             modelBuilder.Entity("Post", b =>
@@ -130,40 +193,23 @@ namespace ForumApp.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("Thread", "Thread")
+                    b.HasOne("ForumThread", "ForumThread")
                         .WithMany("Posts")
-                        .HasForeignKey("ThreadId")
+                        .HasForeignKey("ForumThreadId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
                     b.Navigation("Author");
 
-                    b.Navigation("Thread");
-                });
-
-            modelBuilder.Entity("Thread", b =>
-                {
-                    b.HasOne("User", "Author")
-                        .WithMany()
-                        .HasForeignKey("AuthorId");
-
-                    b.HasOne("Category", "Category")
-                        .WithMany("Threads")
-                        .HasForeignKey("CategoryId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Author");
-
-                    b.Navigation("Category");
+                    b.Navigation("ForumThread");
                 });
 
             modelBuilder.Entity("Category", b =>
                 {
-                    b.Navigation("Threads");
+                    b.Navigation("ForumThreads");
                 });
 
-            modelBuilder.Entity("Thread", b =>
+            modelBuilder.Entity("ForumThread", b =>
                 {
                     b.Navigation("Posts");
                 });
