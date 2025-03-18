f9335e03d814d5692ca60e50372dfac31418ac0a
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2025-03-18 23:56:50 +0000

    Add nullable property to Category.Description

--- ForumApp/Models/Category.cs
+++ ForumApp/Models/Category.cs
@@ -9,7 +9,7 @@ public class Category
 
     [Required]
     [StringLength(100)]
-    public string Description { get; set; }
+    public string? Description { get; set; }
     
     public List<ForumThread> ForumThreads { get; set; } = new List<ForumThread>();
 }
